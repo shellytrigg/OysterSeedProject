c0eb4256065f3fd219fb2a7bef9dfac876d948b2
Kaitlyn Mitchell <mitchellrkaitlyn@gmail.com>
2018-04-26 12:40:35 -0700

    all kmeans files

--- analysis/kmeans/kmeansclustering.R
+++ analysis/kmeans/kmeansclustering.R
@@ -1,11 +1,8 @@
 #K MEANS CLUSTERING
 
 #install packages for vegan and cluster
-install.packages("vegan")
-library(permute, lattice)
-library(vegan)
-install.packages("cluster")
-library(cluster)
+install.packages("vegan", "cluster", "pvclust", "fpc")
+library(vegan, cluster, pvclust, fpc)
 
 #read in file with things you want to cluster (i.e., protein names) as rows and variables you want to cluster across (i.e., NSAF for each day & temperature) as columns
 allsilos <- read.csv('Documents/robertslab/labnotebook/raw_data/ABACUSdata.csv', header=T, row.names=1 )
@@ -18,7 +15,7 @@ silo3 <- allsilos[, (seq(from = 3, to = 22, by = 3))]
 silo9 <- allsilos[, (seq(from = 4, to = 22, by = 3))]
 
 #make a euclidean distance matrix
-silo9.eucd <- vegdist(silo9, method='euclidean')
+silo2.eucd <- vegdist(silo2, method='euclidean')
 
 #look at the scree plot to determine the number of clusters you want to choose. Generally, choose the highest scree value >2
 
@@ -27,17 +24,23 @@ pca_silo9 <- princomp(silo9)
 summary(pca_silo9)
 screeplot(pca_silo9)
 
-#set wd
-setwd('Documents/robertslab/labnotebook/analysis/kmeans/')
-getwd()
-
-#Save scree plot
 jpeg(filename = "scree_s9.jpeg", width = 1000, height = 1000)
 screeplot(pca_silo9)
 dev.off()
 
-#let's say you choose 4 clusters based on the scree plot
+#scree code
+nhclus.scree(silo2.eucd, max.k=7047)
 
+#remove undetected proteins, look at elbow, run kmeans
+nsafcl.s2kmeans <- kmeans(nsaf, tra, centers=3, iter.max=1000, nstart=25)
+
+setwd('Documents/robertslab/labnotebook/analysis/kmeans/')
+getwd()
+jpeg(filename = "nhclus-scree_s2.jpeg", width = 1000, height = 1000)
+nsafcl.kmeans <- kmeans(nsaf, tra, centers=3, iter.max=1000, nstart=25)
+dev.off()
+
+#let's say you choose 4 clusters based on the scree plot
 silo9.4clus <- kmeans(silo9.eucd, centers=4, iter.max=10000, nstart=25)
 silo9.4clus
 
@@ -48,3 +51,6 @@ silo9.4clus
 jpeg(filename = "kmeans_s2.jpeg", width = 1000, height = 1000)
 plot(silo2[c("X2_15", "X2_3")], col = silo2.4clus$cluster)
 dev.off()
+
+
+df$newcolumnname
